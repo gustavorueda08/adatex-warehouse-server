12e691e49aaa52e8a3a9b72f2b6cc2d8c3af825e
adatexco <gerencia@adatex.co>
2025-11-27 16:20:12 -0500

    cors modificados

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,9 +4,23 @@ module.exports = [
     name: "global::error-handler", // Importante: usar el prefijo 'global::'
     config: {},
   },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [
+        "https://www.adatex.com.co",
+        "https://adatex.com.co",
+        "https://adatex-warehouse-production.up.railway.app",
+        "http://localhost:3000",
+      ], // Orígenes permitidos
+      headers: ["Content-Type", "Authorization", "X-Frame-Options"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+      credentials: true,
+    },
+  },
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
