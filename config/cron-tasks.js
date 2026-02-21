b908199b6fa327f6eb7b7b56e522dd5851b799e7
adatexco <gerencia@adatex.co>
2026-02-20 19:21:01 -0500

    Cron desactivado

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -3,9 +3,9 @@ module.exports = {
    * Cron job para verificar y enviar listas de empaque pendientes.
    * Se ejecuta cada 20 minutos para dar tiempo suficiente a revisión en Siigo.
    */
-  "*/20 * * * *": async ({ strapi }) => {
-    await strapi
-      .service("api::order.packing-list-notifier")
-      .processPendingOrders();
-  },
+  // "*/20 * * * *": async ({ strapi }) => {
+  //   await strapi
+  //     .service("api::order.packing-list-notifier")
+  //     .processPendingOrders();
+  // },
 };
